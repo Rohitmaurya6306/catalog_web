38d38546719ada967df1c0faea7031f05a36c758
6306rrmm <marohit789@gmail.com>
2025-06-27 09:32:59 +0530

    Added new file

--- models/Client.js
+++ models/Client.js
@@ -2,7 +2,8 @@ const mongoose = require('mongoose');
 
 const clientSchema = new mongoose.Schema({
     username: { type: String, required: true, unique: true },
-    password: { type: String, required: true }
+    password: { type: String, required: true },
+    // client_id: { type: Number, unique: true,},
 });
 
 module.exports = mongoose.model('Client', clientSchema);
